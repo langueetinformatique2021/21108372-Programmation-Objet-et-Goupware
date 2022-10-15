bd9e8ceb2cb6b7ed4ff8875f43fe9e2be636dac1
yuyan <yuyan.qian22@gmail.com>
2022-10-15 16:51:52 +0200

    changer les annotations

--- TP/src/yuyan_TP04/median_devoir.java
+++ TP/src/yuyan_TP04/median_devoir.java
@@ -8,14 +8,14 @@ public class median_devoir {
 		  * trier et indexer
 		  */
 	
-		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8, p6 ;
-		int[] liste = {p1,p2,p3,p4,p5};
+		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8, p7=3 ,p6 ;
+		int[] liste = {p1,p2,p3,p4,p5,p7}; 
 		Arrays.sort(liste); // list sorted 
 
 		if (liste.length % 2 == 1) {   // index -- median 
 			p6 = liste[ liste.length / 2 ]; 
 		} else {
-			p6 = (liste[ liste.length / 2 - 1] + liste[ liste.length / 2 - 1])/2;
+			p6 = (liste[ liste.length / 2 - 1] + liste[ liste.length / 2 + 1])/2;
 		}   // index de 0; longueur de 1 
 		
 		System.out.println(p6);
