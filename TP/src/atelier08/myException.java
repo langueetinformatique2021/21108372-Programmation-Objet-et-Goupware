4743f44356e6c0f018415569f3d80bb0bdccb02d
yuyan <yuyan.qian22@gmail.com>
2022-11-24 11:41:57 +0100

    atelier 8

--- TP/src/atelier08/myException.java
+++ TP/src/atelier08/myException.java
@@ -2,7 +2,7 @@ package atelier08;
 
 public class myException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;//
 	public myException() {
 	}
 	public myException(String message) {
